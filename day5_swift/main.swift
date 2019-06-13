971e4165dc6aad89dab93dd884ac40f70151c41e
nainaVaghasiya <nainavaghasya9@gmail.com>
2019-06-13 18:15:09 -0400

    first commiit

---
+++ day5_swift/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  day5_swift
+//
+//  Created by MacStudent on 2019-06-13.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+protocol Display {
+    func display()
+}
